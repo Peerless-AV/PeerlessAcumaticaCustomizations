2a4098a568a870972b1ffaa7b2ad0fe2d1c2a6ec
Dan Zabinski <dzabinski@peerless-av.com>
2026-04-08 10:16:33 -0500

    Updates before version  3

--- _Customization Packages/_Peerless_Created_Packages/PRL_SQL_Salesforce/CustomerSync.sql
+++ _Customization Packages/_Peerless_Created_Packages/PRL_SQL_Salesforce/CustomerSync.sql
@@ -107,14 +107,23 @@ CustomerBalanceCTE AS (
 -- ----------------------------------------------------------------
 SalespersonRoles AS (
     SELECT
-        sg.CompanyID,
-        sg.SalespersonCD,
-        sg.Descr AS SalespersonName
-    FROM [dbo].[SalesPerson] sg
+        csp.CompanyID,
+        csp.BAccountID,
+        MAX(CASE WHEN csp.usrSalesPersonGroup = 'AO' THEN sp.Descr END) AS AccountOwner,
+        MAX(CASE WHEN csp.usrSalesPersonGroup = 'CO' THEN sp.Descr END) AS Coordinator,
+        MAX(CASE WHEN csp.usrSalesPersonGroup = 'KD' THEN sp.Descr END) AS KeyDirector,
+        MAX(CASE WHEN csp.usrSalesPersonGroup = 'KM' THEN sp.Descr END) AS KeyManager,
+        MAX(CASE WHEN csp.usrSalesPersonGroup = 'SO' THEN sp.Descr END) AS SalesOperations,
+        MAX(CASE WHEN csp.usrSalesPersonGroup = 'IS' THEN sp.Descr END) AS InsideSales,
+        MAX(CASE WHEN csp.usrSalesPersonGroup = 'CR' THEN sp.Descr END) AS CommissionReceiver
+    FROM [dbo].[CustSalesPeople] csp
+    JOIN [dbo].[SalesPerson] sp
+        ON  sp.SalespersonID = csp.SalesPersonID
+        AND sp.CompanyID     = csp.CompanyID
+    WHERE csp.CompanyID IN (6, 8, 10)
     GROUP BY
-        sg.CompanyID,
-        sg.SalespersonCD,
-        sg.Descr
+        csp.CompanyID,
+        csp.BAccountID
 ),
 
 -- ----------------------------------------------------------------
--- _Customization Packages/_Peerless_Created_Packages/PRL_SQL_Salesforce/Working Directory/SalesPersonLookup.sql
+++ _Customization Packages/_Peerless_Created_Packages/PRL_SQL_Salesforce/Working Directory/SalesPersonLookup.sql
@@ -1,16 +1,18 @@
-SELECT TOP (1000) [CompanyID]
-      ,[SalespersonID]
-      ,[SalespersonCD]
-      ,[CommnPct]
-      ,[SalesSubID]
-      ,[tstamp]
-      ,[CreatedByID]
-      ,[CreatedByScreenID]
-      ,[CreatedDateTime]
-      ,[LastModifiedByID]
-      ,[LastModifiedByScreenID]
-      ,[LastModifiedDateTime]
-      ,[Descr]
-      ,[IsActive]
-      ,[NoteID]
-  FROM [2024R2DEV].[dbo].[SalesPerson]
+    SELECT
+        csp.CompanyID,
+        csp.BAccountID,
+        MAX(CASE WHEN csp.usrSalesPersonGroup = 'AO' THEN sp.Descr END) AS AccountOwner,
+        MAX(CASE WHEN csp.usrSalesPersonGroup = 'CO' THEN sp.Descr END) AS Coordinator,
+        MAX(CASE WHEN csp.usrSalesPersonGroup = 'KD' THEN sp.Descr END) AS KeyDirector,
+        MAX(CASE WHEN csp.usrSalesPersonGroup = 'KM' THEN sp.Descr END) AS KeyManager,
+        MAX(CASE WHEN csp.usrSalesPersonGroup = 'SO' THEN sp.Descr END) AS SalesOperations,
+        MAX(CASE WHEN csp.usrSalesPersonGroup = 'IS' THEN sp.Descr END) AS InsideSales,
+        MAX(CASE WHEN csp.usrSalesPersonGroup = 'CR' THEN sp.Descr END) AS CommissionReceiver
+    FROM [dbo].[CustSalesPeople] csp
+    JOIN [dbo].[SalesPerson] sp
+        ON  sp.SalespersonID = csp.SalesPersonID
+        AND sp.CompanyID     = csp.CompanyID
+    WHERE csp.CompanyID IN (6, 8, 10)
+    GROUP BY
+        csp.CompanyID,
+        csp.BAccountID
